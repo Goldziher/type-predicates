d2d241a064ac2b9a2b603a6e48ac598095a413b9
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-05-25 15:08:55 +0200

    chore: updated packaging

--- vite.config.ts
+++ vite.config.ts
@@ -11,15 +11,23 @@ export default defineConfig({
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
-            fileName: 'index',
+            fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
             formats: ['es', 'cjs'],
             name,
         },
         minify: true,
+        rollupOptions: {
+            output: {
+                preserveModules: false,
+            },
+        },
+        sourcemap: true,
+        target: 'es2020',
     },
     plugins: [dts({ rollupTypes: true })],
     test: {
         coverage: {
+            exclude: ['node_modules/**', 'dist/**'],
             reporter: ['text', 'lcov'],
         },
         globals: true,
